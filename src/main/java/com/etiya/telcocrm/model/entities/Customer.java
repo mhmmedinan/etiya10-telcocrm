48b0753d3287e8b97c93ade5aa452e2b600ec298
mhmmedinan <minan.65@hotmail.com>
2025-10-06 15:12:21 +0300

    add entity

---
+++ src/main/java/com/etiya/telcocrm/model/entities/Customer.java
@@ -0,0 +1,35 @@
+package com.etiya.telcocrm.model.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Customer {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "customer_number")
+    private String customerNumber;
+
+    @PrePersist
+    public void generateCustomerNumber(){
+        String prefix = "CUST-";
+        String year = String.valueOf(java.time.Year.now().getValue());
+        String randomPart = String.format("%04",new java.util.Random().nextInt(10000));
+        this.customerNumber = prefix + year + "-" + randomPart;
+    }
+}
---
+++ src/main/java/com/etiya/telcocrm/model/entities/IndividualCustomer.java
@@ -0,0 +1,46 @@
+package com.etiya.telcocrm.model.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "individual_customers")
+@PrimaryKeyJoinColumn(name = "customer_id")
+public class IndividualCustomer extends Customer {
+
+
+    @Column(name = "firstName",nullable = false,length = 50)
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "middleName")
+    private String middleName;
+
+    @Column(name = "nationalId",unique = true,updatable = false,length = 11)
+    private String nationalId;
+
+    @Column(name = "dateOfBirth")
+    private LocalDateTime dateOfBirth;
+
+    @Column(name = "motherName")
+    private String motherName;
+
+    @Column(name = "fatherName")
+    private String fatherName;
+
+    @Column(name = "gender")
+    private String gender;
+
+}
