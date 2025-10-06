c8849e0cf680f7fdbdd7b91607fe1954c5ff3f30
mhmmedinan <minan.65@hotmail.com>
2025-10-06 13:46:59 +0300

    first

---
+++ src/main/java/com/etiya/telcocrm/TelcoCrmApplication.java
@@ -0,0 +1,13 @@
+package com.etiya.telcocrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etiya/telcocrm/TelcocrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiya.telcocrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcocrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
